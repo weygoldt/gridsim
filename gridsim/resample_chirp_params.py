be09feb82f73230cb5c22f681aa9b7957800ec7a
weygoldt <88969563+weygoldt@users.noreply.github.com>
2023-05-26 10:42:44 +0200

    save chirp simulation parameters

--- gridsim/resample_chirp_params.py
+++ gridsim/resample_chirp_params.py
@@ -9,7 +9,7 @@ import pathlib
 
 import matplotlib.pyplot as plt
 import numpy as np
-from eod.communication import chirp_model
+from extract_chirp_params import chirp_model
 from IPython import embed
 from rich import print
 from scipy.interpolate import interp1d
@@ -18,55 +18,76 @@ from scipy.signal.windows import tukey
 
 def load_dataset(path: pathlib.Path):
     freq_fits = list(path.glob("*freq_fit.npy"))
-    env_fits = list(path.glob("*env_fit.npy"))
 
-    print(f"Found {len(env_fits)} files.")
+    print(f"Found {len(freq_fits)} files.")
 
     freq_params = []
-    env_params = []
-    for freq, env in zip(freq_fits, env_fits):
+    for freq in freq_fits:
         fps = np.load(freq)
-        eps = np.load(env)
         freq_params.append(fps)
-        env_params.append(eps)
     freq_params = np.concatenate(freq_params)
-    env_params = np.concatenate(env_params)
-    return freq_params, env_params
+    return freq_params
+
+
+def sort_dataset(dataset):
+    """
+    Sort the Gaussians in each chirp by their amplitude.
+    """
+
+    sorted_dataset = np.zeros_like(dataset)
+
+    for i in range(len(dataset)):
+        curr_fit = dataset[i, :]
+
+        # split fits into 3 gaussians
+        split_fit = np.split(curr_fit, 3)
+
+        # sort by height, which is the second parameter of each gaussian
+        sorted_fit = sorted(split_fit, key=lambda x: x[1])
+
+        # put it back into the matrix
+        sorted_dataset[i, :] = np.concatenate(sorted_fit)
+
+    return sorted_dataset
+
+
+def remove_outliers(dataset):
+    """
+    Remove all parameter arrays that have large outliers.
+    """
+
+    # get indices of rows that contain outliers in any parameter
+    for i in range(dataset.shape[1]):
+        param = dataset[:, i]
+        outliers = np.abs(param - np.mean(param)) > 2.5 * np.std(param)
+        dataset = dataset[~outliers, :]
+
+    return dataset
 
 
 def resample_chirp_params(path: pathlib.Path):
-    freq_fits, env_fits = load_dataset(path)
+    freq_fits = load_dataset(path)
+    # freq_fits = sort_dataset(freq_fits)
+
+    # remove the nans from the dataset
+    freq_fits = freq_fits[~np.isnan(freq_fits).any(axis=1)]
+
+    freq_fits = remove_outliers(freq_fits)
 
     params = [
-        "amp1",
-        "std1",
-        "mu1",
-        "kurt1",
-        "amp2",
-        "std2",
-        "mu2",
-        "kurt2",
-        "amp3",
-        "std3",
-        "mu3",
-        "kurt3",
+        "m1",
+        "h1",
+        "w1",
+        "k1",
+        "m2",
+        "h2",
+        "w2",
+        "k2",
+        "m3",
+        "h3",
+        "w3",
+        "k3",
     ]
-
-    # remove the nan colums in both parameter matrices
-    freq_fits = freq_fits[:, ~np.isnan(freq_fits).any(axis=0)]
-    env_fits = env_fits[:, ~np.isnan(env_fits).any(axis=0)]
-
-    # sort by std of first gaussian of freq
-    env_fits = env_fits[np.argsort(freq_fits[:, 1])]
-    freq_fits = freq_fits[np.argsort(freq_fits[:, 1])]
-
-    # remote outliers in all dimensions
-    # for i in range(fits.shape[1]):
-    #     fits = fits[np.abs(fits[:, i] - np.mean(fits[:, i])) < 10 * np.std(fits[:, i])]
-
-    #     # specifically remove too high kurt2 values
-    #     if i == 7:
-    #         fits = fits[np.abs(fits[:, i] - np.mean(fits[:, i])) < np.std(fits[:, i])]
 
     # interpolate to make more
     old_x = np.arange(freq_fits.shape[0])
@@ -76,58 +97,78 @@ def resample_chirp_params(path: pathlib.Path):
     new_env_fits = []
 
     for i in range(freq_fits.shape[1]):
+        print(i)
         ff = interp1d(old_x, freq_fits[:, i], kind="linear", fill_value="extrapolate")
-        ef = interp1d(old_x, env_fits[:, i], kind="linear", fill_value="extrapolate")
 
         new_freq_fits.append(ff(new_x))
-        new_env_fits.append(ef(new_x))
 
-        plt.plot(new_x, ff(new_x), label="freq")
-        plt.plot(new_x, ef(new_x), label="env")
+        plt.plot(new_x, ff(new_x), ".", label="freq")
+        plt.plot(old_x, freq_fits[:, i], ".", label="old freq")
 
         plt.title(params[i])
         plt.legend()
         plt.show()
 
     new_freq_fits = np.array(new_freq_fits).T
-    new_env_fits = np.array(new_env_fits).T
 
     # plot the new distributions and overlay the old ones
-    fig, ax = plt.subplots(4, 4, figsize=(20, 20))
-    for i, param in enumerate(params):
-        ax[i // 4, i % 4].hist(freq_fits[:, i], bins=100, alpha=0.5, label="freq")
-        ax[i // 4, i % 4].hist(new_freq_fits[:, i], bins=100, alpha=0.5, label="new freq")
-        ax[i // 4, i % 4].set_title(param)
-    fig.suptitle("Frequency fits")
-    plt.tight_layout()
-    plt.show()
+    param_names = [
+        "Center",
+        "Height",
+        "Width",
+        "Kurtosis",
+    ]
 
-    # repeat for env
-    fig, ax = plt.subplots(4, 4, figsize=(20, 20))
-    for i in range(len(params)):
-        ax[i // 4, i % 4].hist(env_fits[:, i], bins=100, alpha=0.5, label="env")
-        ax[i // 4, i % 4].hist(new_env_fits[:, i], bins=100, alpha=0.5, label="new env")
-        ax[i // 4, i % 4].set_title(params[i])
-    fig.suptitle("Envelope fits")
-    plt.tight_layout()
+    fig, ax = plt.subplots(3, 4, figsize=(20, 20), sharex="col", sharey="row")
+    for i, param in enumerate(params):
+        ax[i // 4, i % 4].hist(freq_fits[:, i], bins=100, alpha=0.5, label="old")
+        ax[i // 4, i % 4].hist(new_freq_fits[:, i], bins=100, alpha=0.5, label="new")
+        ax[i // 4, i % 4].axvline(0, color="k", ls="--", lw=0.5)
+
+        if i == 0:
+            ax[i // 4, i % 4].legend()
+
+        # add row labels
+        if i % 4 == 0:
+            ax[i // 4, i % 4].set_ylabel("Density")
+
+        # add column labels
+        if i // 4 == 2:
+            ax[i // 4, i % 4].set_xlabel("Parameter value")
+
+        # add column titles
+        if i < 4:
+            ax[i // 4, i % 4].set_title(param_names[i % 4])
+
+    fig.suptitle("Frequency fits")
+    plt.savefig(path / "interpolation_distributions.png")
     plt.show()
 
     # plot the resulting chirps
     fig, ax = plt.subplots()
     t = np.linspace(0, 0.5, 20000) - 0.25
     tuk = tukey(len(t), alpha=0.4)
-    for i in range(len(new_fits)):
-        popt = new_fits[i]
+    for i in range(len(new_freq_fits)):
+        popt = new_freq_fits[i, :]
         chirp = chirp_model(t, *popt)
         mode = np.histogram(chirp, bins=100)[1][
             np.argmax(np.histogram(chirp, bins=100)[0])
         ]
         chirp -= mode
         chirp *= tuk
-        ax.plot(t, chirp, alpha=0.1, color="k")
+        ax.plot(t, chirp, alpha=0.05, color="k", lw=0.5)
+
+    ax.axvline(0, color="k", ls="--", lw=0.5)
+    ax.axhline(0, color="k", ls="--", lw=0.5)
+    ax.set_xlim(-0.1, 0.1)
+    ax.set_ylim(-50, 400)
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Frequency (Hz)")
+    ax.set_title("Interpolated chirp simulations")
+    plt.savefig(path / "interpolation.png")
     plt.show()
 
-    np.save(path / "interpolation.npy", new_fits)
+    np.save(path / "interpolation.npy", new_freq_fits)
 
 
 def interface():
